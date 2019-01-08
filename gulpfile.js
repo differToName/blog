fc35b4309f372973bf8da4104ae98ebf5dd5488f
differToName <bigyyl@qq.com>
2019-01-08 18:15:25 +0800

    2019-01-08 18:15:18

--- gulpfile.js
+++ gulpfile.js
@@ -45,4 +45,5 @@ gulp.task('listenPages', function() {
 });  
 // 构建任务流  
 // 执行 task build  
-gulp.task('build', ['html', 'image-min','minify-js']);
+gulp.task('build',gulp.series('html', 'image-min','minify-js',function(){
+}));
